c0d97bda7dca6565c446e80303c1eff8cce0c79f
danielJing <danieljing1993@gmail.com>
2022-01-29 11:51:41 +1100

    add validate for login form and add form title

---
+++ pages/login/index.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+export const StyledLoginTitle = styled(Title)`
+  text-align: center;
+  margin: 0.5em 0;
+  margin-top: 2em;
+  font-size: 18px !important;
+  padding-bottom: 0;
+`;
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -4,6 +4,7 @@ import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Role, LoginForm } from './types';
 import Header from '../../components/Header';
 import { useRouter } from 'next/router';
+import { StyledLoginTitle } from './index.style';
 
 const Login: React.FC = () => {
   const router = useRouter();
@@ -21,8 +22,10 @@ const Login: React.FC = () => {
   return (
     <>
       <Header />
+
       <Row justify="center">
         <Col md={8} lg={8} sm={24}>
+          <StyledLoginTitle>Course Management Assistant</StyledLoginTitle>
           <Form name="login" onFinish={onFinish} autoComplete="off">
             <Form.Item name="role" initialValue={Role.student}>
               <Radio.Group>
@@ -31,12 +34,31 @@ const Login: React.FC = () => {
                 <Radio.Button value={Role.manager}>Manager</Radio.Button>
               </Radio.Group>
             </Form.Item>
-            <Form.Item name="email" rules={[{ required: true, type: 'email', message: '' }]}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  type: 'email',
+                  message: 'The input is not valid E-mail!',
+                },
+                {
+                  required: true,
+                  message: 'Please input your E-mail!',
+                },
+              ]}
+            >
               <Input prefix={<UserOutlined />} placeholder="Please Input Email" />
             </Form.Item>
             <Form.Item
               name="password"
-              rules={[{ required: true, min: 0, max: 16, message: 'Please input your Password!' }]}
+              rules={[
+                {
+                  required: true,
+                  min: 4,
+                  max: 16,
+                  message: 'password must be between 4 and 16 characters',
+                },
+              ]}
             >
               <Input
                 prefix={<LockOutlined />}
@@ -48,6 +70,8 @@ const Login: React.FC = () => {
               <Button type="primary" htmlType="submit" block>
                 Log in
               </Button>
+            </Form.Item>
+            <Form.Item>
               No Accounts <a href="#/">Sign Up</a>
             </Form.Item>
           </Form>
